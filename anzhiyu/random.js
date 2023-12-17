8437d958de4463162e9d5aa5e3cff3e97449cdd3
Flumbrix1145 <zongshiwuzhao@126.com>
2023-12-17 20:21:41 +0800

    Site updated: 2023-12-17 20:21:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/e732d5f6.html","posts/minifier.html","posts/36679002.html","posts/869eb1f7.html","posts/7dfef69c.html","posts/df7635ee.html","posts/3464e4f.html","posts/npminstall.html","posts/99c9062b.html","posts/5d36ff15.html","posts/113bdaa7.html","posts/22966c79.html","posts/a5115977.html","posts/596431e6.html","posts/35aa5673.html","posts/Fengling.html","posts/714d1047.html"];function toRandomPost(){
+var posts=["posts/e732d5f6.html","posts/minifier.html","posts/36679002.html","posts/869eb1f7.html","posts/7dfef69c.html","posts/df7635ee.html","posts/99c9062b.html","posts/3464e4f.html","posts/npminstall.html","posts/22966c79.html","posts/113bdaa7.html","posts/a5115977.html","posts/596431e6.html","posts/35aa5673.html","posts/Fengling.html","posts/714d1047.html","posts/5d36ff15.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
