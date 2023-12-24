9946f1a8c02d24fc3287782a437372f4f5d740f3
Flumbrix1145 <zongshiwuzhao@126.com>
2023-12-24 10:13:38 +0800

    Site updated: 2023-12-24 10:13:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/githubsc.html","posts/minifier.html","posts/hexoycwz.html","posts/hexozlj.html","posts/linuxrm.html","posts/dloby.html","posts/mdyf.html","posts/TED1.html","posts/alist.html","posts/npminstall.html","posts/algoliasearch.html","posts/ruanjian.html","posts/jianzhan.html","posts/githubfangwen.html","posts/huizhou.html","posts/Fengling.html","posts/chemistry.html"];function toRandomPost(){
+var posts=["posts/githubsc.html","posts/minifier.html","posts/hexozlj.html","posts/hexoycwz.html","posts/dloby.html","posts/linuxrm.html","posts/mdyf.html","posts/TED1.html","posts/alist.html","posts/algoliasearch.html","posts/chemistry.html","posts/ruanjian.html","posts/npminstall.html","posts/githubfangwen.html","posts/jianzhan.html","posts/huizhou.html","posts/Fengling.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
